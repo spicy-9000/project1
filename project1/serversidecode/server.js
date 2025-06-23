108df7da5ad9f01975d4ed42aae4ea689248d00e
spicy-9000 <spicyq48@gmail.com>
2025-06-23 20:31:56 +0530

    server.js

--- project1/serversidecode/server.js
+++ project1/serversidecode/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors');//install cors and express
 const fs = require('fs');
 const mysql = require('mysql2');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
+const nodemailer = require('nodemailer');// install nodemailer
+const crypto = require('crypto');//needs npm intsall crypto
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -19,26 +19,26 @@ const db = mysql.createConnection({
 });
 
 db.connect(err => {
-  if (err) console.error('DB Error:', err);
-  else console.log(' Connected to MySQL');
+  if (err) console.error('DB Error:', err);//gives error
+  else console.log(' Connected to MySQL');//else connects
 });
 
-app.post('/signin', (req, res) => {
+app.post('/signin', (req, res) => {//endpath at signin to store post
   const { uname, semail, phone, gender, password } = req.body;
-  const sql = "INSERT INTO users (uname, semail, phone, gender, password) VALUES (?, ?, ?, ?, ?)";
+  const sql = "INSERT INTO users (uname, semail, phone, gender, password) VALUES (?, ?, ?, ?, ?)";//inserting details
   db.query(sql, [uname, semail, phone, gender, password], (err, result) => {
     if (err) return res.status(500).send('DB Error: ' + err.message);
-    res.send('User registered successfully');
+    res.send('User registered successfully');//inserts the details into database users from login
   });
 });
 
 app.post('/forgot-password', (req, res) => {
-  const { email } = req.body;
+  const { email } = req.body;// if user forgets password
 
   const checkQuery = "SELECT * FROM users WHERE semail = ?";
-  db.query(checkQuery, [email], (err, results) => {
+  db.query(checkQuery, [email], (err, results) => {//query and email as args
     if (err) return res.status(500).send("DB error");
-    if (results.length === 0) return res.status(404).send("User not found");
+    if (results.length === 0) return res.status(404).send("User not found");//if no results not matching with the given email at login then gives user is not found 
 
     const token = crypto.randomBytes(32).toString("hex");
     const updateQuery = "UPDATE users SET reset_token = ? WHERE semail = ?";
@@ -65,7 +65,7 @@ app.post('/forgot-password', (req, res) => {
       };
 
       transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
+        if (err) {//give if not mail coudlnt be send
           console.error("Mail error:", err);
           return res.status(500).send("Error sending email");
         }
@@ -74,7 +74,7 @@ app.post('/forgot-password', (req, res) => {
     });
   });
 });
-app.get('/api/plants', (req, res) => {
+app.get('/api/plants', (req, res) => {// anothe database regarding palnts which will fetch from the uploads for displaying in home page
   db.query('SELECT * FROM plants', (err, results) => {
     if (err) return res.status(500).send(err);
     res.json(results);
@@ -85,4 +85,3 @@ app.get('/api/plants', (req, res) => {
 app.listen(4002, () => {
   console.log('🌱 Server running at http://localhost:4002');
 });
-
